abdbdfab29adb19f11d0545852f660e825117732
Darth Ares <angela198341@gmail.com>
2025-04-22 14:29:47 +0600

    12345

--- obj/Debug/net8.0/Identity.AssemblyInfo.cs
+++ obj/Debug/net8.0/Identity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcde16bf770d6607bd20bf5ddbb25ec915df71d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a91e73ad7b971f99c16be60b6400d1573f3bd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
